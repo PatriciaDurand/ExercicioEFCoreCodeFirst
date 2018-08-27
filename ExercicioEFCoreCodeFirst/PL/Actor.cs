da6f709e6d6515ce11323666fb19b8656fe24b5c
TAMIRES DA SILVA DOMINGUES <11204785@pucrs.br>
2018-08-27 20:31:34 -0300

    Alteração Aula 2708 Inclusão Actor, ActorMovie

---
+++ ExercicioEFCoreCodeFirst/PL/Actor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioEFCoreCodeFirst.PL
+{
+    class Actor
+    {
+        public int ActorId { get; set; }
+        public DateTime DateBirth { get; set; }
+        public string Name { get; set; }
+       // public ICollection<Movie> Movies { get; set; }
+    }
+}
---
+++ ExercicioEFCoreCodeFirst/PL/ActorMovie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioEFCoreCodeFirst.PL
+{
+    class ActorMovie
+    {
+        public int ActorId { get; set; }
+        public string Character { get; set; }
+        public int MovieId { get; set; }
+        public ICollection<Actor> Actors { get; set; }
+    }
+}
